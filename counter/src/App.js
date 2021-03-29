0c84c65ea4827227c94650f68c3a19bcf9e23c12
kirilllan <s2100138@edu.bc.fi>
2021-03-29 14:19:26 +0300

    step3

--- counter/src/App.js
+++ counter/src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import "./App.css"
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
+
+addHandler = () => {this.setState({
+  counter: this.state.counter+1
+})}
+removeHandler = () => {this.setState({
+  counter: this.state.counter -1
+})}
+resetHandler = () => {this.setState({
+  counter: 0
+})}
   render() {
     return (
       <div>
-        <h1 className="circle">0</h1>
+        <h1 className="circle">{this.state.counter}</h1>
+        <button onClick={this.addHandler}>+ Add 1 +</button>
+        <button onClick={this.removeHandler}>- Remove 1 -</button>
+        <button onClick={this.resetHandler}>|reset|</button>
       </div>
     )
   }
