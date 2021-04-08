e133e51da4f4678d0a984b85b2eb6d70f3f174cd
kirilllan <s2100138@edu.bc.fi>
2021-04-08 14:49:51 +0300

    commit 2 Apr_08

--- react-ereaction-game/src/App.js
+++ react-ereaction-game/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {Component} from 'react';
+import Circle from "./Circle";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    circles: [
+      {id: 1, color: 'green'},
+      {id:2,color:'yellow'},
+      {id:3,color:'red'},
+      {id:4,color:'orange'}
+    ]
+  }
+  render() {
+    const circleList = this.state.circles.map(c => {
+      return <Circle id={c.id} key={c.id} color={c.color}/>
+    }); 
+    return (
+      <div>
+        <h1>Speedgame</h1>
+        <p>Your score is:</p>
+        <div className="circles">{circleList}</div>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    )
+}
 }
 
 export default App;
