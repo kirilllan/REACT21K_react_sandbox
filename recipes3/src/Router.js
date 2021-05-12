5a945bc5908eca81cd139c68a94bba78c9689780
kirilllan <s2100138@edu.bc.fi>
2021-05-12 14:50:42 +0300

    functional,needs styling

--- recipes3/src/Router.js
+++ recipes3/src/Router.js
@@ -1,38 +1,39 @@
-import React, {useContext} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useContext } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AuthHome from "./auth/AuthHome";
-import NewRecipe from "./recipe/NewRecipe";
 import UserContext from "./context/UserContext";
-import RecipeList from "./recipe/RecipeList";
-import Recipe from "./recipe/RecipeList";
-import RecipeNavbar from "./recipe/RecipeNavbar";
 import EditRecipe from "./recipe/EditRecipe";
+import NewRecipe from "./recipe/NewRecipe";
+import Recipe from "./recipe/Recipe";
+import RecipeList from "./recipe/RecipeList";
+import RecipeNavbar from "./recipe/RecipeNavbar";
 
 const Router = () => {
   const user = useContext(UserContext);
+
   return (
     <BrowserRouter>
       <Switch>
         {user ? (
-        <>
-        <RecipeNavbar />
-          <Route exact path="/">
-            <RecipeList />
-          </Route>
-          <Route path="/new">
-            <NewRecipe />
-          </Route>
-          <Route path="/recipe/:id" component={Recipe} />
-          <Route path="/edit/id" component={EditRecipe} />
-        </>
+          <>
+            <RecipeNavbar />
+            <Route exact path="/">
+              <RecipeList />
+            </Route>
+            <Route path="/new">
+              <NewRecipe />
+            </Route>
+            <Route path="/recipe/:id" component={Recipe} />
+            <Route path="/edit/:id" component={EditRecipe} />
+          </>
         ) : (
-        <>
-          <AuthHome />   
-        </>
+          <>
+            <AuthHome />
+          </>
         )}
       </Switch>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
--- recipes3/src/recipe/Recipe.js
+++ recipes3/src/recipe/Recipe.js
@@ -1,50 +1,69 @@
-import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import UserContext from "../context/UserContext";
 import { firestore } from "../firebase";
 
-export default function Recipe(props) {
+const Recipe = (props) => {
   const [recipe, setRecipe] = useState(undefined);
+
   const user = useContext(UserContext);
+
   const history = useHistory();
+
   const deleteRecipe = async () => {
-    await firestore.collection("users").doc(user.uid).collection("recipes").doc(props.match.params.id).delete();
-    history.push('/');
-  }
+    await firestore
+      .collection("users")
+      .doc(user.uid)
+      .collection("recipes")
+      .doc(props.match.params.id)
+      .delete();
+    history.push("/");
+  };
+
   useEffect(() => {
     const getRecipe = async () => {
-      const recipeDoc = await firestore.collection("users").doc(user.uid).collection("recipes").doc(props.match.params.id).get();
+      const recipeDoc = await firestore
+        .collection("users")
+        .doc(user.uid)
+        .collection("recipes")
+        .doc(props.match.params.id)
+        .get();
+
       setRecipe(recipeDoc);
     };
     getRecipe();
   }, [user, props.match.params.id]);
+
   const renderIngredients = () => {
     const recipeData = recipe.data();
-    return recipeData.ingredients.map((ingredient, idx) => {
-      return ( <>
-      <li key={i}>{ingredient}</li>
-      <p>{recipeData.description}</p>
-      </> )
-    })
-  }
+    return recipeData.ingredients.map((ingredient, i) => {
+      return <li key={i}>{ingredient}</li>;
+    });
+  };
+
   const renderRecipe = () => {
     const recipeData = recipe.data();
-      return ( <>
-      <h2>{recipeData.name}</h2>
-      <ul>
-        {renderIngredients}
-      </ul>
-    </> )
+    console.log(recipeData.description);
+    return (
+      <>
+        <h2>{recipeData.name}</h2>
+        <ul>{renderIngredients()}</ul>
+        <pre>
+          <p>{recipeData.description}</p>
+        </pre>
+      </>
+    );
   };
+
   return (
-    <div classNAme="recipe">
-      <h1>Recipes:</h1>
+    <div className="recipe">
       {recipe && renderRecipe()}
-    <button onClick={deleteRecipe}>Delete</button>
-    <button onClick={() => history.push(`&edit/${props.match.params.id}`)}>Edit</button>
+      <button onClick={() => history.push(`/edit/${props.match.params.id}`)}>
+        Edit
+      </button>
+      <button onClick={deleteRecipe}>Delete</button>
     </div>
-  )
-}
+  );
+};
 
-
-
+export default Recipe;
