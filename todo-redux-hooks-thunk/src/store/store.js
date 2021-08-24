7e9c82045c42feb592e6cdad431a22e600d544a8
kirilllan <s2100138@edu.bc.fi>
2021-08-24 14:41:02 +0300

    update store.js & new package

--- todo-redux-hooks-thunk/src/store/store.js
+++ todo-redux-hooks-thunk/src/store/store.js
@@ -1,7 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
-const store = createStore(reducer, composeWithDevTools())
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
