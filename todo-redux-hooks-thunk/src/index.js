cd56b67694e8b71820966e3131daeb8f374129be
kirilllan <s2100138@edu.bc.fi>
2021-08-24 14:25:10 +0300

    whole store to separate file

--- todo-redux-hooks-thunk/src/index.js
+++ todo-redux-hooks-thunk/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-import reducer from './store/reducers/reducer';
-const store = createStore(reducer, composeWithDevTools())
+import store from './store/store'
 
 ReactDOM.render(
   <Provider store={store}>
