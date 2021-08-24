4b4962858cb4d5972a1ccd0b7e3c784dd08275ce
kirilllan <s2100138@edu.bc.fi>
2021-08-24 12:00:10 +0300

    add redux,store,provider

--- todo-redux-hooks-thunk/src/index.js
+++ todo-redux-hooks-thunk/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore()
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
