4c19cba0546aca6c3a371183466d8bf2a196cb77
kirilllan <s2100138@edu.bc.fi>
2021-08-24 13:04:04 +0300

    dispatching function in form onsubmit

--- todo-redux-hooks-thunk/src/containers/NewNote.js
+++ todo-redux-hooks-thunk/src/containers/NewNote.js
@@ -5,11 +5,22 @@ import { useSelector, useDispatch } from 'react-redux';
 
 const NewNote = () => {
   const notes = useSelector(state => state)
+  const dispatch = useDispatch()
+  const addTodo = text => ({
+    type: actionTypes.ADD_TODO,
+    id: i++,
+    text: text,
+    completed: false
+  })
   let i = notes.length
 
   return (
-    <form>
-      <input type="text" name="note"></input>
+    <form onSubmit={e => {
+      e.preventDefault()
+      dispatch(addTodo(e.target.noteInput.value))
+      e.target.note.value = ''
+    }}>
+      <input type="text" name="noteInput"></input>
       <input type="submit" value="Add note"></input>
     </form>
   );
