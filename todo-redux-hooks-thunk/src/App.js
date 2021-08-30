e72a566bf298e31057e3eabcdbe044cc6308fe02
kirilllan <s2100138@edu.bc.fi>
2021-08-30 12:48:44 +0300

    func 4 creating new note with axios

--- todo-redux-hooks-thunk/src/App.js
+++ todo-redux-hooks-thunk/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NewNote from './containers/NewNote'
 import Notes from './containers/Notes'
-
+import { useEffect } from 'react';
 import { initializeNotes } from './store/reducers/reducer'
 import { useDispatch } from 'react-redux'
 
