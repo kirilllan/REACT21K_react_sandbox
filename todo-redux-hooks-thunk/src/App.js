db9f0dc4e6b6a4e5977b7b7020672ada74728f4d
kirilllan <s2100138@edu.bc.fi>
2021-08-30 10:42:47 +0300

    dispatching in App.js

--- todo-redux-hooks-thunk/src/App.js
+++ todo-redux-hooks-thunk/src/App.js
@@ -2,7 +2,14 @@ import './App.css';
 import NewNote from './containers/NewNote'
 import Notes from './containers/Notes'
 
+import { initializeNotes } from './store/reducers/reducer'
+import { useDispatch } from 'react-redux'
+
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeNotes())
+  }, [dispatch])
   return (
     <div className="App">
       <NewNote />
