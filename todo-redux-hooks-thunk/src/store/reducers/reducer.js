f65a2321829d37421996563a54a19c60ef56e2ff
kirilllan <s2100138@edu.bc.fi>
2021-08-30 14:37:59 +0300

    removing from DB & state working =3

--- todo-redux-hooks-thunk/src/store/reducers/reducer.js
+++ todo-redux-hooks-thunk/src/store/reducers/reducer.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions'
 import noteServices from '../../services/Notes'
+import axios from 'axios'
 
 const reducer = (state = [], action) => {
   switch (action.type) {
@@ -8,14 +9,14 @@ const reducer = (state = [], action) => {
     case actionTypes.ADD_TODO:
       return [...state, action.data]
     case actionTypes.TOGGLE_TODO:
-      console.log('from toggletodo')
       const noteToChange = state.find(n => n.id === action.id)
       const changeNote = { ...noteToChange, completed: !noteToChange.completed }
       return state.map((note) => (note.id !== action.id ? note : changeNote))
     case actionTypes.REMOVE:
-      console.log('from remove:', action, action.removeName)
-      //return state
-      return state.filter(note => note.text !== action.removeName)
+      return state.filter(note => {
+        if (note.text !== action.removeName) return note
+        else axios.delete('http://localhost:3001/notes/' + note.id)
+      })
   }
   return state
 }
