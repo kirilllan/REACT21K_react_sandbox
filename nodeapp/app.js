dbcf4f1b4bd69f56ab7fe15d2ee03c9e39bc12b3
kirilllan <s2100138@edu.bc.fi>
2021-09-09 10:54:14 +0300

    init things

---
+++ nodeapp/app.js
@@ -0,0 +1,11 @@
+const { application } = require('express')
+const express = require('express')
+const app = express()
+
+app.get('/names', (req, res) => {
+  res.send('James')
+})
+
+app.listen(5000, () => {
+  console.log('server is running on port 5000')
+})
