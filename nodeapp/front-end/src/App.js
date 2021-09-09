e2f4325e0fb03bfd719572d92337abff800a4da3
kirilllan <s2100138@edu.bc.fi>
2021-09-09 11:04:43 +0300

    base things done

--- nodeapp/front-end/src/App.js
+++ nodeapp/front-end/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [userName, setUsername] = useState('')
+  useEffect(() => {
+    getNames()
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names')
+    console.log('response: ', response)
+  }
+
+
   return (
     <div className="App">
-
+      from App.js
     </div>
   );
 }
